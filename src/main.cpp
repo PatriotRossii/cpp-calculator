00bad8ae62f58722d5cef2314da6eebd47b11219
PatriotRossii <patriotrossii2019@mail.ru>
2021-11-01 16:00:51 +0500

    Make it look like an interpreter

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,25 @@
 #include "Visitor.hpp"
 
 using namespace std;
+using namespace antlr4;
 
 int main(int argc, const char* argv[]) {
-    std::ifstream stream;
-    stream.open("input.txt");
-    
-    antlr4::ANTLRInputStream input(stream);
-    MathLexer lexer(&input);
-    antlr4::CommonTokenStream tokens(&lexer);
-    MathParser parser(&tokens);    
+    std::string instruction;
 
-    MathParser::ExprContext* tree = parser.expr();
+    std::cout << "> ";
+    while(std::getline(std::cin, instruction)) {
+        ANTLRInputStream stream(instruction);
 
-    Visitor visitor;
-    double result = visitor.visit(tree).as<double>();
-    std::cout << result;
+        MathLexer lexer(&stream);
+        antlr4::CommonTokenStream tokens(&lexer);
+        MathParser parser(&tokens);
+
+        MathParser::ExprContext* tree = parser.expr();
+
+        Visitor visitor;
+        double result = visitor.visit(tree).as<double>();
+        std::cout << result << "\n> ";
+    }
 
     return 0;
 }
