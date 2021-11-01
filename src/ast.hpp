f742699c505bbeccad7b0b79583153501d436152
PatriotRossii <patriotrossii2019@mail.ru>
2021-11-01 18:57:56 +0500

    Modify AST to use CalculatorState

--- src/ast.hpp
+++ src/ast.hpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <string>
 
+#include "calculator/state.hpp"
+
 enum class BinaryOperation {
 	ADD, SUB, DIV, MUL
 };
@@ -9,12 +11,16 @@ enum class BinaryOperation {
 class ExprAST {
 public:
 	virtual ~ExprAST() { }
+	virtual double eval(CalculatorState&) = 0;
 };
 
 class IntegerExprAST: public ExprAST {
 	double value;
 public:
 	IntegerExprAST(double value): value(value) {}
+	double eval(CalculatorState&) override {
+		return value;
+	}
 };
 
 class BinaryExprAST: public ExprAST {
@@ -24,6 +30,9 @@ public:
 	BinaryExprAST(BinaryOperation operation,
 		std::unique_ptr<ExprAST> LHS, std::unique_ptr<ExprAST> RHS)
 	: operation(operation), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+	double eval(CalculatorState& state) override {
+		return LHS->eval(state) + RHS->eval(state);
+	}
 };
 
 class CallExprAST: public ExprAST {
@@ -33,4 +42,13 @@ public:
 	CallExprAST(const std::string& name,
 		std::vector<std::unique_ptr<ExprAST>> args)
 	: name(name), args(std::move(args)) { }
+	double eval(CalculatorState& state) override {
+		std::vector<double> values;
+		std::transform(args.begin(), args.end(),
+			std::back_inserter(values), [&state](const std::unique_ptr<ExprAST>& arg) {
+				return arg->eval(state);
+			}
+		);
+		return state.invoke(name, values);
+	}
 };
